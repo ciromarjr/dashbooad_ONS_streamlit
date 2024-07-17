dd14dcab58983ef3317ead427c44d4e46d01d37d
Ciromar Junior <ciromarjr@gmail.com>
2024-07-17 18:04:17 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,8 @@ import plotly.figure_factory as ff
 from datetime import datetime
 import time
 
-
 # Função para obter dados e cachear por 20 segundos
+
 @st.cache_data(ttl=20)
 def get_data(url):
     response = requests.get(url)
